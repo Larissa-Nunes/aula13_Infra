1e9515364b55d97ad2a11887d6c9ddb4ed913fa9
Larissa <nuneslari21@gmail.com>
2021-11-17 17:32:52 -0300

    first commit

---
+++ src/test/java/dev/larissa/jenkinsspringdocker/SpringjenkinsdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.larissa.jenkinsspringdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjenkinsdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
